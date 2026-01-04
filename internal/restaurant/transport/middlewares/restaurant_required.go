ca062327fd76c9ee9729ec3da0ab053f3a8be3f4
seno-collab <senonguyen2109@gmail.com>
2026-01-04 17:44:30 +0700

    Update restaurant_required.go

--- internal/restaurant/transport/middlewares/restaurant_required.go
+++ internal/restaurant/transport/middlewares/restaurant_required.go
@@ -1,43 +1,43 @@
-package middlewares
+// package middlewares
 
-import (
-	"go-ai/internal/identity/domain/rbac"
-	repo "go-ai/internal/restaurant/infrastructure/db"
-	"go-ai/internal/transport/response"
-	"net/http"
+// import (
+// 	"go-ai/internal/identity/domain/rbac"
+// 	repo "go-ai/internal/restaurant/infrastructure/db"
+// 	"go-ai/internal/transport/response"
+// 	"net/http"
 
-	"github.com/google/uuid"
-	"github.com/labstack/echo/v4"
-)
+// 	"github.com/google/uuid"
+// 	"github.com/labstack/echo/v4"
+// )
 
-type RestaurantRequired struct {
-	Sqlc *repo.RestaurantRepo
-}
+// type RestaurantRequired struct {
+// 	queries *repo.RestaurantRepo
+// }
 
-func New(sqlc *repo.RestaurantRepo) *RestaurantRequired {
-	return &RestaurantRequired{
-		Sqlc: sqlc,
-	}
-}
+// func New(queries *repo.RestaurantRepo) *RestaurantRequired {
+// 	return &RestaurantRequired{
+// 		queries: queries,
+// 	}
+// }
 
-func (r *RestaurantRequired) Handler(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		userID := c.Get("user_id")
-		role := c.Get("role")
-		if userID == nil {
-			return response.Error(c, 401, "Unauthorized access")
-		}
-		userUUID, ok := userID.(uuid.UUID)
-		if !ok {
-			return response.Error(c, http.StatusForbidden, "You do not have permission to access this resource")
-		}
-		if role != rbac.Admin {
-			id, err := r.Sqlc.GetRestaurantByUserID(c.Request().Context(), userUUID)
-			if err != nil {
-				return response.Error(c, http.StatusForbidden, "You do not have permission to access this resource")
-			}
-			c.Set("restaurant_id", id)
-		}
-		return next(c)
-	}
-}
+// func (r *RestaurantRequired) Handler(next echo.HandlerFunc) echo.HandlerFunc {
+// 	return func(c echo.Context) error {
+// 		userID := c.Get("user_id")
+// 		role := c.Get("role")
+// 		if userID == nil {
+// 			return response.Error(c, 401, "Unauthorized access")
+// 		}
+// 		userUUID, ok := userID.(uuid.UUID)
+// 		if !ok {
+// 			return response.Error(c, http.StatusForbidden, "You do not have permission to access this resource")
+// 		}
+// 		if role != rbac.Admin {
+// 			id, err := r.queries.GetRestaurantByUserID(c.Request().Context(), userUUID)
+// 			if err != nil {
+// 				return response.Error(c, http.StatusForbidden, "You do not have permission to access this resource")
+// 			}
+// 			c.Set("restaurant_id", id)
+// 		}
+// 		return next(c)
+// 	}
+// }
